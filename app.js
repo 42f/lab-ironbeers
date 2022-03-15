44b5dbc046567b29038c746248e5869ba03757b1
bvalette <bvalette@student.42.fr>
2022-03-15 17:14:55 +0100

    done - mandatory part

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -7,6 +8,11 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+app.use(
+  require('connect-livereload')({
+    port: 3001,
+  })
+)
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +26,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', async (req, res) => {
+  try {
+    const beersDataFromApi = await punkAPI.getBeers();
+    if (beersDataFromApi) {
+      console.log(beersDataFromApi);
+      const data = {
+        beers: beersDataFromApi
+      }
+      res.render('beers', data);
+    }
+  } catch (err) {
+    // render an error page ?
+    console.log(err)
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    if (randomBeer && randomBeer.length) {
+      const data = {
+        beer: randomBeer[0]
+      }
+      console.log(randomBeer);
+      res.render('random-beer',data);
+    }
+  } catch (error) {
+
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
